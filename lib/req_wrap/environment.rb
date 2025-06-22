75c136fb144f0a0d010fd7f88ac6017856b9d15d
vxd7 <vxd732@protonmail.com>
2025-06-22 17:12:14 +0500

    fix(v0.1.0): better validation for Environment
    Do not raise error when request env file is not supplied at all for
    request execution

--- lib/req_wrap/environment.rb
+++ lib/req_wrap/environment.rb
@@ -18,18 +18,18 @@ module ReqWrap
 
     def initialize(env_file = nil)
       @env_file = env_file || default_env_file
-
-      raise ArgumentError, 'Env file not supplied' unless @env_file
-      raise ArgumentError, 'Env file does not exist' unless File.exist?(@env_file)
     end
 
     def load
+      return unless validate
       return load_str_environment(decrypt) if encrypted?
 
       Dotenv.load(@env_file)
     end
 
     def write_encrypted_environment(delete_original: false)
+      validate(raise_error: true)
+
       encrypted_file_for("#{@env_file}#{ENCRYPTED_ENV_FILE_EXT}").write(
         File.read(@env_file, mode: 'rb:BOM|utf-8')
       )
@@ -38,6 +38,8 @@ module ReqWrap
     end
 
     def decrypt
+      validate(raise_error: true)
+
       raise ArgumentError, 'Trying to decrypt non-encrypted env file' unless encrypted?
 
       encrypted_file_for(@env_file).read
@@ -70,6 +72,22 @@ module ReqWrap
         env_key: 'REQWRAP_PASSWORD',
         raise_if_missing_key: true
       )
+    end
+
+    def validate(raise_error: false)
+      unless @env_file
+        return false unless raise_error
+
+        raise ArgumentError, 'Env file not supplied'
+      end
+
+      unless File.exist?(@env_file)
+        return false unless raise_error
+
+        raise ArgumentError, 'Env file does not exist'
+      end
+
+      true
     end
   end
 end
